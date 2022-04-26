01be0829884fcb95693cbaefed6b2ca6911ce1fe
Denis Levy <denis.levi11@gmail.com>
2022-04-26 19:20:26 -0300

    004 - Fix placeholder messages

--- src/components/RegisterFormComponent/index.jsx
+++ src/components/RegisterFormComponent/index.jsx
@@ -8,10 +8,10 @@ export const RegisterFormComponent = () => (
     <FormRegister action="">
       <H1Component>Registrar</H1Component>
       <InputComponent id="fullName" labelText="Nome completo" required type="text" variant='dark' placeholder="João da silva" labelColor='#e2eaff'/>
-      <InputComponent id="CPF" labelText="CPF" required type="number" variant='dark' labelColor='#e2eaff'/>
-      <InputComponent id="RG" labelText="RG" required type="number" variant='dark' labelColor='#e2eaff'/>
-      <InputComponent id="cellphone" labelText="Celular" type="number" variant='dark' labelColor='#e2eaff'/>
-      <InputComponent id="email" labelText="Email" type="email" variant='dark' labelColor='#e2eaff'/>
+      <InputComponent id="CPF" labelText="CPF" required type="number" variant='dark' labelColor='#e2eaff' placeholder="123.456.789-10"/>
+      <InputComponent id="RG" labelText="RG" required type="number" variant='dark' labelColor='#e2eaff' placeholder="1234567"/>
+      <InputComponent id="cellphone" labelText="Celular" type="number" variant='dark' labelColor='#e2eaff' placeholder="(12) 91324-1234"/>
+      <InputComponent id="email" labelText="Email" type="email" variant='dark' labelColor='#e2eaff' placeholder="exemplo@exemplo.com"/>
       <InputComponent id="birthday" labelText="Data de nascimento" required type="date" variant='dark' labelColor='#e2eaff'/>
       <InputComponent id="password" labelText="Senha" required type="text" variant='dark' labelColor='#e2eaff'/>
       <ButtonComponent textLabel="Registrar" bgcolor='white' color='#3e66ff' variant='dark' labelColor='#e2eaff'/>
