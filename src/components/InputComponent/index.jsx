f515d428b6da156904de9897fec9c48850fca1ad
Denis Levy <denis.levi11@gmail.com>
2022-04-26 19:03:42 -0300

    004 - Added register component

--- src/components/InputComponent/index.jsx
+++ src/components/InputComponent/index.jsx
@@ -1,9 +1,9 @@
 import { InputComponentContainer, InputComponentStyle, InputHelperText, InputLabel } from './styles';
 
-export const InputComponent = ({id,labelText, variant='light', helperText, ...others}) => (
+export const InputComponent = ({id,labelText, variant='light', helperText, required, ...others}) => (
   <InputComponentContainer>
-    <InputLabel htmlFor={id}>{labelText}</InputLabel>
-    <InputComponentStyle id={id} variant={variant} {...others}/>
+    <InputLabel htmlFor={id}>{labelText}{required ? '*' : ''}</InputLabel>
+    <InputComponentStyle id={id} name={id} variant={variant} required={required} {...others}/>
     {helperText && <InputHelperText>{helperText}</InputHelperText>}
   </InputComponentContainer>
 )
--- src/components/InputComponent/styles.js
+++ src/components/InputComponent/styles.js
@@ -9,27 +9,33 @@ export const InputComponentStyle = styled.input`
     background-color: transparent;
     border-radius: 0.6rem;
 
-    border: 1px solid ${variant === 'light' ? '#b6b5bc' : '#b6cdff'};
-    color: ${variant === 'light' ? '#67719a' : '#b6b5bc'};
+    border: 1px solid ${variant === 'light' ? '#b6b5bc' : '#a2beff'};
+    color: ${variant === 'light' ? '#67719a' : '#e2eaff'};
 
     padding: 0.5rem;
     font-size: 1rem;
 
     outline: none;
     &:focus {
-      border-color: #67719a;
+      border-color: #a2beff;
+    }
+
+    &::placeholder{
+      color: ${variant === 'light' ? '#706e7f' : '#a5c2ff'};
     }
   `}
 `;
 
 export const InputLabel = styled.label`
   font-weight: 700;
+  margin-bottom: 0.3rem;
 
 
-  color: #6b6587;
+  color: #e2eaff;
 `
 
 export const InputHelperText = styled.p`
   font-size: 15px;
+  margin-top: 0.2rem;
   color: #ae4646;
 `
