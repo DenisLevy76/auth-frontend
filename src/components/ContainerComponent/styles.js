f1c38f647c008bf8da16e76b4af4669e97fa7795
Denis Levy <denis.levi11@gmail.com>
2022-04-26 17:51:42 -0300

    005 - Added Container component

---
+++ src/components/ContainerComponent/styles.js
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components';
+
+export const ContainerWrapper = styled.section`
+  ${({bgColor}) => css`
+    background-color: ${bgColor};
+    padding: 2rem;
+    border-radius: 1.5rem;
+    box-shadow: 0px 0px 10px rgba(17.6, 12.9, 54.1, 0.5);
+  `}
+`
