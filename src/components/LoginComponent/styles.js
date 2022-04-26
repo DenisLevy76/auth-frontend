b07d6e907705264effb8ad9f95ea350c3ae3039a
Gabriel Borges <gabrieelb.13@gmail.com>
2022-04-26 19:26:49 -0300

    003 - Adding login component

---
+++ src/components/LoginComponent/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const FormularioLogin = styled.form`
+    display: flex;
+    flex-direction: column ;
+    gap: 1rem;
+`;
+
+export const TextoLogin = styled.h1`
+    margin-top: 1rem;
+    margin-bottom: 3rem;
+    color: #2D218A;
+    font-weight: 700;
+    font-size: 2rem;
+    align-self: center;
+`;
