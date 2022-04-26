9c9aacf57f17b61a7db14e2b219cfc4b1206f38f
Denis Levy <denis.levi11@gmail.com>
2022-04-26 19:13:23 -0300

    004 - fix InputLabel color

--- src/components/RegisterFormComponent/index.jsx
+++ src/components/RegisterFormComponent/index.jsx
@@ -7,14 +7,14 @@ export const RegisterFormComponent = () => (
   <ContainerComponent bgColor='#3e66ff'>
     <FormRegister action="">
       <H1Component>Registrar</H1Component>
-      <InputComponent id="fullName" labelText="Nome completo" required type="text" variant='dark' placeholder="João da silva"/>
-      <InputComponent id="CPF" labelText="CPF" required type="number" variant='dark'/>
-      <InputComponent id="RG" labelText="RG" required type="number" variant='dark'/>
-      <InputComponent id="cellphone" labelText="Celular" type="number" variant='dark'/>
-      <InputComponent id="email" labelText="Email" type="email" variant='dark'/>
-      <InputComponent id="birthday" labelText="Data de nascimento" required type="date" variant='dark'/>
-      <InputComponent id="password" labelText="Senha" required type="text" variant='dark'/>
-      <ButtonComponent textLabel="Registrar" bgcolor='white' color='#3e66ff' variant='dark'/>
+      <InputComponent id="fullName" labelText="Nome completo" required type="text" variant='dark' placeholder="João da silva" labelColor='#e2eaff'/>
+      <InputComponent id="CPF" labelText="CPF" required type="number" variant='dark' labelColor='#e2eaff'/>
+      <InputComponent id="RG" labelText="RG" required type="number" variant='dark' labelColor='#e2eaff'/>
+      <InputComponent id="cellphone" labelText="Celular" type="number" variant='dark' labelColor='#e2eaff'/>
+      <InputComponent id="email" labelText="Email" type="email" variant='dark' labelColor='#e2eaff'/>
+      <InputComponent id="birthday" labelText="Data de nascimento" required type="date" variant='dark' labelColor='#e2eaff'/>
+      <InputComponent id="password" labelText="Senha" required type="text" variant='dark' labelColor='#e2eaff'/>
+      <ButtonComponent textLabel="Registrar" bgcolor='white' color='#3e66ff' variant='dark' labelColor='#e2eaff'/>
     </FormRegister>
   </ContainerComponent>
 )
