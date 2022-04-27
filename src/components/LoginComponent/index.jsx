50ff407c5696ddfacd47da911cb6c29bd180fd7f
gabriel borges <pomppeji@gmail.com>
2022-04-27 18:31:22 -0300

    Adding useState to login form

--- src/components/LoginComponent/index.jsx
+++ src/components/LoginComponent/index.jsx
@@ -3,17 +3,46 @@ import { ContainerComponent } from "../ContainerComponent";
 import { InputComponent } from "../InputComponent";
 import { FormularioLogin, TextoLogin } from "./styles";
 import {Link} from 'react-router-dom';
+import { useState } from "react";
 
-export const LoginComponent = () => (
-   <ContainerComponent>
-        <FormularioLogin>
-            <TextoLogin>Log in</TextoLogin>
-            <InputComponent id="CPF" labelText="CPF" placeholder="digite seu CPF" type="text"/>
-            <InputComponent id="password" labelText="Senha" placeholder="digite sua Senha" type="text"/>
-            <ButtonComponent textLabel="Entrar"/>
-            <Link to="/register">
-                <ButtonComponent textLabel="Não possui conta? Cadastrar-se" bgcolor="transparent" color="#6a6490"/>
-            </Link>
-        </FormularioLogin>
-    </ContainerComponent>
-)
+export const LoginComponent = () => {
+
+    function mostrar(e) {
+        e.preventDefault()
+        console.log(CPF)
+        console.log(senha)
+    }
+
+    const [CPF, setCPF] = useState()
+    const [senha, setSenha] = useState()
+
+    return (
+
+        <ContainerComponent>
+            <FormularioLogin onSubmit={mostrar}>
+                <TextoLogin>Log in</TextoLogin>
+
+                <InputComponent 
+                id="CPF" 
+                onChange={(e) => setCPF(e.target.value)}
+                labelText="CPF" 
+                placeholder="digite seu CPF" 
+                type="text"
+                />
+
+                <InputComponent 
+                id="password" 
+                onChange={(e) => setSenha(e.target.value)}
+                labelText="Senha" 
+                placeholder="digite sua Senha" 
+                type="password"
+                />
+
+                <ButtonComponent textLabel="Entrar"/>
+                <Link to="/register">
+                    <ButtonComponent textLabel="Não possui conta? Cadastrar-se" bgcolor="transparent" color="#6a6490"/>
+                </Link>
+            </FormularioLogin>
+        </ContainerComponent>
+    )
+}
